fed6d302b536564381f52de72559410896e05d3d
Jonas Greitemann <jgreitemann@gmail.com>
2016-05-14 14:51:35 +0200

    Updated showcase / integration program vec

--- vec.cpp
+++ vec.cpp
@@ -22,27 +22,24 @@ using namespace std;
 using namespace Vec;
 
 int main(int argc, char *argv[]) {
-    vec<3,double> a = {1., 2., 3.};
-    vec<3,double> b = {3,  2,  1 };
-    cout << a.norm() << endl;
-    vec<3,double> d = a - b;
+    // default data type assumes double
+    vec<3> a = {1, 2, 3};
+    // 2-norm
+    cout << "||a|| = " << a.norm() << endl;
+    vec<3> b = {3, 2, 1};
+    cout << "dot product: a * b = " << a * b << endl;
+    // cross product is defined on vec<3,T> only
+    cout << "cross product: a x b = " << cross(a, b) << endl;
+    // linear operations are overloaded, naturally
+    vec<3> d = a - b;
     d /= 2;
-    cout << "d = (" << d[0] << "," << d[1] << "," << d[2] << ")" << endl;
-    cout << d.norm() << endl;
-    vec<3> e(a);
-    cout << (a == b) << endl;
-    cout << (a != b) << endl;
-    cout << (a == e) << endl;
-    cout << (a != e) << endl;
-    vec<3> f = cross(a, b);
-    cout << "(" << f[0] << ", " << f[1] << ", " << f[2] << ")" << endl;
-    cout << (f * a) << endl;
+    cout << "d = " << d << endl;
 
     complex<int> I(0, 1);
-    vec<2,complex<int>> c {1+I, 2-I};
+    // data type may be complex and integral
+    vec<2, complex<int>> c = {1+I, 2-I};
+    // norm, dot product, and cross product are complex-aware
     cout << c.norm() << endl;
-    c.conj();
-    cout << "(" << c[0] << "," << c[1] << ")" << endl;
-    cout << c.norm() << endl;
+    cout << "conjugate: " << conj(c) << endl;
     return 0;
 }
