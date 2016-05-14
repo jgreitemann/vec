37bec8ac4bfc63d602b9936d5eceb238c53b1a0c
Jonas Greitemann <jgreitemann@gmail.com>
2016-05-14 14:07:33 +0200

    Added a swap test
    Did not actually need to implement anything for this one. swap is not
    explicitly overloaded for vec as no move semantics are implemented at
    this point. This is more of a regression test.

---
+++ tests/swap.cpp
@@ -0,0 +1,38 @@
+/*  vec -- small physical vectors, complex or real
+ *  Copyright (C) 2016  Jonas Greitemann <j.greitemann@lmu.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <cassert>
+#include <limits>
+#include <utility>
+#include "../vec.hpp"
+
+using namespace Vec;
+
+const size_t N = 10;
+
+int main () {
+    vec<N,double> a, b;
+    for (size_t i = 0; i < N; ++i) {
+        a[i] = double(i);
+        b[i] = double(N-i);
+    }
+    vec<N,double> c(a), d(b);
+    std::swap(a, b);
+    assert(a == d);
+    assert(b == c);
+
+    return 0;
+}
